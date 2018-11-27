7994ff548b682ea888af143d5129cd24da45f63e
Michael Salvati <print@3daddict.com>
2018-11-27 09:03:24 -0800

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,8 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain('Hi There!')
+
   ReactDOM.unmountComponentAtNode(div);
 });
