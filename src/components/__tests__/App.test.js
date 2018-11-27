212540a8406699b5986018af6fcd0a35164bdf98
Michael Salvati <print@3daddict.com>
2018-11-27 10:51:17 -0800

    test function cleaned up

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
     const wrapped = shallow(<App />);
-
     expect(wrapped.find(CommentBox).length).toEqual(1);
+    expect(wrapped.find(CommentList).length).toEqual(1);
 });
