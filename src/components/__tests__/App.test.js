f3bf31bd7f8bb7f7513229fda83f7f65678defe8
Michael Salvati <print@3daddict.com>
2018-11-27 10:37:07 -0800

    enzyme test example created

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+    const wrapped = shallow(<App />);
 
-    ReactDOM.render(<App />, div);
-
-    //Looks inside the div to check and see if the comment box is in there.
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
+    expect(wrapped.find(CommentBox).length).toEqual(1);
 });
